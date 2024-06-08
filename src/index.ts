c22323801934a470582911873117882def460fae
Argha Das <arghadas@Arghas-MacBook-Air.local>
2024-06-08 18:59:05 +0530

    parse

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://mern-booking-app-frontend-ten.vercel.app",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
